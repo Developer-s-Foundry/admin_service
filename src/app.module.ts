cfaacca211700b1e12ddd2e20ed08b6b326c8ddf
DANNURA <nurudeendanjuma93@gmail.com>
2025-04-30 00:32:15 +0100

    adminModulesUpdates

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
-// Removed duplicate definition
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DatabaseModule } from './database/database.module';
-import { AdminModule } from './admin/admin.module'; // Import AdminModule
+import { AdminModule } from './admin/admin.module';
 
 @Module({
-  imports: [DatabaseModule, AdminModule], // Add AdminModule here
+  imports: [DatabaseModule, AdminModule],
   controllers: [AppController],
   providers: [AppService],
 })
